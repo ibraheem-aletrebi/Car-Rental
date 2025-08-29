e4c0f61066353cd4344582edb38b04a569ac2d57
Ibraheem Al-Etrebi <ibraheemaletrebi@gmail.com>
2025-08-29 18:42:37 +0300

    finish on boarding and splash screen for androide up/down 12

--- lib/app/car_rental.dart
+++ lib/app/car_rental.dart
@@ -1,6 +1,7 @@
-
 import 'package:car_rental/core/routing/on_generate_route.dart';
 import 'package:car_rental/core/routing/routes.dart';
+import 'package:car_rental/core/services/local_services/preference_manager.dart';
+import 'package:car_rental/core/services/local_services/storage_key.dart';
 import 'package:flutter/material.dart';
 
 class CarRental extends StatelessWidget {
@@ -12,7 +13,13 @@ class CarRental extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       onGenerateRoute: onGenerateRoute,
-      initialRoute: Routes.kOnboardingView,
+      initialRoute: getInitialRoute(),
     );
+  }
+
+  String getInitialRoute() {
+    return PreferenceManegar().getBool(StorageKey.shownOnBoarding) ?? false
+        ? Routes.kMainView
+        : Routes.kOnboardingView;
   }
 }
